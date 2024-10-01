9c15ba1f0829f00bb17a5e2849b0512832a82873
TalittaAmorim <talitasilva.web@gmail.com>
2024-10-01 08:05:48 -0300

    first commit

---
+++ rotas/jobs.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../models/Job');
+
+router.get('/test', (req,res) => {
+    res.send('deu certo');
+});
+
+// form da rota de envio
+router.get('/add',(req, res) => {
+    res.render('add');
+})
+
+// adiciona job via post
+router.post('/add', (req,res) => {
+
+    let {title, salary, company, description, email, new_job} = req.body;
+
+    // inserir
+    Job.create({
+        title,
+        description,
+        salary,
+        company,
+        email,
+        new_job
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err));
+});
+
+// datlhe da vaga -> view/1, view/2
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: {id: req.params.id}
+}).then(job => {
+
+    res.render('view', {
+        job
+    });
+}).catch(err => console.log(err)))
+
+module.exports = router
